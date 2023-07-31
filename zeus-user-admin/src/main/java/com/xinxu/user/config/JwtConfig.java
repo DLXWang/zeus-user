5479c056a2d8bc2104108411524bfcf2cc65e3b8
“alexwang” <wx3a07@gmail.com>
2023-07-31 21:42:21 +0800

    <commit>

--- zeus-user-admin/src/main/java/com/xinxu/user/config/JwtConfig.java
+++ zeus-user-admin/src/main/java/com/xinxu/user/config/JwtConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,6 +20,7 @@ public class JwtConfig implements WebMvcConfigurer {
     private String private_key;
     private long access_token_expiration_time;
     private long refresh_token_expiration_time;
+    private boolean enable_swagger;
 
     @Resource
     @Lazy
@@ -26,9 +28,10 @@ public class JwtConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new JwtCheckFilter(userCenterApi))
-                .excludePathPatterns("/swagger-resources/**","/v2/api-docs")
-                .order(Ordered.LOWEST_PRECEDENCE);
-
+        InterceptorRegistration interceptorRegistration = registry.addInterceptor(new JwtCheckFilter(userCenterApi));
+        if (enable_swagger) {
+            interceptorRegistration.excludePathPatterns("/swagger-resources/**", "/v2/api-docs")
+                    .order(Ordered.LOWEST_PRECEDENCE);
+        }
     }
 }
