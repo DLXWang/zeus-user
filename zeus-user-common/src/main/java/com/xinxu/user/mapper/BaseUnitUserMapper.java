8a963819a76c50ebd2264cfbc1c827016d9163ca
“alexwang” <wx3a07@gmail.com>
2023-07-31 19:56:15 +0800

    <commit>

--- zeus-user-common/src/main/java/com/xinxu/user/mapper/BaseUnitUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.xinxu.user.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BaseUnitUserMapper extends BaseMapper <BaseUnitUser>{
-}
